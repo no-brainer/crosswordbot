3af2a668394ce12c26b913b4df52d0e67e80a58e
Polina Guseva <p.guseva00@gmail.com>
2020-05-23 21:40:19 +0300

    initial commit

--- crossbot/bot.py
+++ crossbot/bot.py
@@ -53,7 +53,7 @@ def on_ans(update, context):
         update.message.reply_markdown_v2(settings.INCORRECT_FORMAT_MSG)
         return ConversationState.WAITING_ANSWERS
     try:
-        logger.log(context.ars)
+        logger.log(context.args)
         context.chat_data[StoredValue.CROSSWORD_STATE].set_answer(*context.args)
     except ValueError:
         update.message.reply_text(settings.ANSWER_TOO_LONG_MSG)
